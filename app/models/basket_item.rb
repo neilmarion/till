14afc0d339a930fc22efa292ffc0a3c5113df5e1
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2025-08-13 13:53:00 +0800

    Update BasketItem

--- app/models/basket_item.rb
+++ app/models/basket_item.rb
@@ -1,4 +1,17 @@
 class BasketItem < ApplicationRecord
   belongs_to :basket
   belongs_to :product
+
+  before_validation :snapshot_price, on: :create
+
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+
+  def price_money
+    Money.new((price.to_d * 100).round, "EUR")
+  end
+
+  private
+  def snapshot_price
+    self.price = product.price if product
+  end
 end
