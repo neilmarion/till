791b0fd95c3a588a15a0b785fe3dcce246eabf26
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2025-08-14 09:40:31 +0800

    Update seeds to be the same to the exam sample

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,12 +10,13 @@ Product.create!(
 # SR1 Strawberry (bulk discount: 4+ items -> 60% off each)
 Product.create!(
   product_code: 'SR1', name: 'Strawberry', price: 5.00,
-  discount_rule: :bulk_discount, discount_threshold: 4, discount_percentage: 60
+  discount_rule: :bulk_discount, discount_threshold: 3, discount_percentage: 10
 )
 
 # CF1 Coffee (no discount)
 Product.create!(
-  product_code: 'CF1', name: 'Coffee', price: 11.23
+  product_code: 'CF1', name: 'Coffee', price: 11.23,
+  discount_rule: :bulk_discount, discount_threshold: 3, discount_percentage: 33.3333
 )
 
 puts "Seeded: #{Product.count} products"
